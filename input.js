b977326837dc565c2662a1d719eb523dbddc9840
mborroni <milagros.borroni@mercadolibre.com>
2019-12-23 13:27:26 -0300

    change getKeyword function and schema props

--- input.js
+++ input.js
@@ -2,18 +2,22 @@ const prompt = require('prompt');
 
 const keywordSchema = {
   properties: {
-    name: {
+    keyword: {
       type: 'string',
-      message: 'Search by keyword',
       required: true
     }
   }
 };
 
 const getKeyword = callback => {
-  prompt.get(keywordSchema, callback);
+  prompt.get(keywordSchema, (err, res) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+
+    callback(res.keyword);
+  });
 };
 
 module.exports = { getKeyword };
-
-setKeyword(searchJokes);
